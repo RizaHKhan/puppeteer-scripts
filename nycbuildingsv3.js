31cd1747677d63aef1bf95ca7b36b8d1cd43f755
RizaHKhan <khanriza@gmail.com>
2020-11-12 21:23:38 -0500

    adjusted script to higher order

--- nycbuildingsv3.js
+++ nycbuildingsv3.js
@@ -52,88 +52,38 @@ const crawler = async (url) => {
 };
 
 const one = crawler(
-  "http://a810-bisweb.nyc.gov/bisweb/ECBQueryByNumberServlet?requestid=20&ecbin=38236472X"
-  // "http://a810-bisweb.nyc.gov/bisweb/ECBQueryByNumberServlet?requestid=20&ecbin=39016173H"
+  // "http://a810-bisweb.nyc.gov/bisweb/ECBQueryByNumberServlet?requestid=20&ecbin=38236472X"
+  "http://a810-bisweb.nyc.gov/bisweb/ECBQueryByNumberServlet?requestid=20&ecbin=39016173H"
   // "http://a810-bisweb.nyc.gov/bisweb/ECBQueryByNumberServlet?requestid=20&ecbin=35150318L"
 );
 
 let objectsArray = [];
 
+function splitByColon(item, array) {
+  let obj = {};
+  const key = item.split(":")[0];
+  const val = item.split(":")[1].trim();
+  obj[key] = val;
+  array.push(obj);
+}
+
+function byNextInArray(key, value, array) {
+  let obj = {};
+  obj[key] = value.trim();
+  array.push(obj);
+}
+
 Promise.all([one]).then((values) => {
   const one = values[0];
-  values[0].forEach((item) => {
-    item.split("\t").map((item, index, array) =>
-      item
-        .split("\n")
-        .filter((item) => item.length)
-        .forEach((item) => {
-          let isTrimmed = item.trim();
-          let isSplit = isTrimmed.split("/t");
-          let isSplitByColon = isSplit.toString().split(":");
-          let obj = {};
-          // Items that split into pairs
-          if (
-            isTrimmed.includes("Premises") ||
-            isTrimmed.includes("Filed At") ||
-            isTrimmed.includes("Community Board") ||
-            isTrimmed.includes("OATH/ECB Violation Number") ||
-            isTrimmed.includes("DOB Violation Number") ||
-            isTrimmed.includes("Issued as Aggravated Level")
-          ) {
-            obj[isSplitByColon[0]] = isSplitByColon[1]
-              ? isSplitByColon[1].trim()
-              : "Not available";
-            objectsArray.push(obj);
-            // Stuff to ignore
-          } else if (
-            isTrimmed.includes("A Certificate of Correction must be") ||
-            isTrimmed.includes("OATH/ECB Violation Summary") ||
-            isTrimmed.includes("Violation Details") ||
-            isTrimmed.includes("OATH/ECB Hearing Information") ||
-            isTrimmed.includes("OATH/ECB Penalty Information") ||
-            isTrimmed.includes("BIN") ||
-            isTrimmed.includes(
-              "Dept. of Buildings Compliance History and Events"
-            ) ||
-            isTrimmed.includes("Respondent Information")
-          ) {
-          } else if (isTrimmed.includes("Scheduled Hearing Date/Time")) {
-            obj[isTrimmed.split(/:(.+)/)[0]] = isTrimmed
-              .split(/:(.+)/)[1]
-              .trim();
-            objectsArray.push(obj);
-            // Singles
-          } else if (
-            isTrimmed.includes("Name") ||
-            isTrimmed.includes("Served Date") ||
-            isTrimmed.includes("Hearing Status") ||
-            isTrimmed.includes("Issuing Inspector ID") ||
-            isTrimmed.includes("Certification Status") ||
-            isTrimmed.includes("Violation Date") ||
-            isTrimmed.includes("Mailing Address") ||
-            isTrimmed.includes("Violation Type") ||
-            isTrimmed.includes("Inspection Unit") ||
-            isTrimmed.includes("Compliance On") ||
-            isTrimmed.includes("Specific Violation Condition") ||
-            isTrimmed.includes("Amount Paid") ||
-            isTrimmed.includes("Adjustments") ||
-            isTrimmed.includes("Penalty Imposed") ||
-            isTrimmed.includes("Penalty Balance Due")
-          ) {
-            obj[array[index]] = array[index + 1];
-            objectsArray.push(obj);
-          } else if (
-            isTrimmed.includes("Infraction Code") ||
-            isTrimmed.includes("")
-          ) {
-            console.log(isTrimmed.split("\n"));
-            obj[array[index]] = array[index + 4];
-            objectsArray.push(obj);
-          } else {
-            console.log(isTrimmed);
-          }
-        })
-    );
+  values[0].forEach((i, index, array) => {
+    // splits into 3
+    if (i.includes("Premises") || i.includes("OATH/ECB Violation Number")) {
+      i.split("\t").forEach((item) =>
+        item.length ? splitByColon(item, objectsArray) : ""
+      );
+    } else {
+      console.log(i.split("\t"));
+    }
   });
   console.log(objectsArray);
 });
